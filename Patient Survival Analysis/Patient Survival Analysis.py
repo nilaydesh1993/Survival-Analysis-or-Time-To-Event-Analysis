6e7ad6e2e22d29109acc08f800b43dbb0be7b2ad
Nilay Deshmukh <60890829+nilaydeshmukh0@users.noreply.github.com>
2020-06-03 19:37:35 +0530

    Add files via upload

---
+++ Patient Survival Analysis/Patient Survival Analysis.py
@@ -0,0 +1,30 @@
+"""
+Created on Mon May 18 20:32:10 2020
+@author: DESHMUKH
+SURVIVAL ANALYSIS
+"""
+# pip install lifelines
+import pandas as pd
+from lifelines import KaplanMeierFitter
+
+# =================================================================================
+# Business Detail - Perform Kaplan meir analysis for the given data and get the life table
+# =================================================================================
+
+patient = pd.read_csv('Patient.csv')
+patient.head()
+patient.info()
+
+# Summary
+patient.describe()
+
+# Initiating the KaplanMeierFitter model
+kmf = KaplanMeierFitter(label='FollowUps vs Event')
+
+# Fitting KaplanMeierFitter model on Followups and Event type
+kmf.fit(patient.Followup, patient.Eventtype) # fit(time,events)
+
+# Time-line estimations plot 
+kmf.plot(color = 'g')
+
+                    # ---------------------------------------------------- #
